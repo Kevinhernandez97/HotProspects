37b6936b38d039a1615d9c796eb541b3607030df
Kevin Hernandez <kmhs-18@hotmail.com>
2023-05-04 16:13:34 -0300

    Initial Commit

---
+++ HotProspects/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HotProspects
+//
+//  Created by Kevin Hernandez on 4/05/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Kevin Hernandez on 4/05/23.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
